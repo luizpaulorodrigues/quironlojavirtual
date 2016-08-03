d9201e97cbff5e0bad163cc54ff7d9c0aad4cde9
Luiz Paulo Cabral Rodrigues <rodrigues.luiz.paulo@hotmail.com>
2016-08-02 21:23:28 -0300

    02/08/2016

---
+++ Quiron.LojaVirtual.Dominio/Respositorio/EFDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Respositorio
+{
+    public class EFDbContext : DbContext
+    {
+        
+    }
+}
